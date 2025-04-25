e91d4f1613af7bcf2183d07f3343ddebdd8a2758
tinh ly <lytinhqqpy@gmail.com>
2025-04-25 14:53:01 +0700

    chang mouse cursor into tab

--- src/public/js/dashboardScript.js
+++ src/public/js/dashboardScript.js
@@ -1,30 +1,30 @@
 
-$(document).ready(function () {
-    $('.toggle-password').click(function () {
-        const targetId = $(this).data('target');
-        const passwordInput = $('#' + targetId);
+// $(document).ready(function () {
+//     $('.toggle-password').click(function () {
+//         const targetId = $(this).data('target');
+//         const passwordInput = $('#' + targetId);
 
-        if (passwordInput.attr('type') === 'password') {
-            passwordInput.attr('type', 'text');
-            $(this).find('i').removeClass('fa-eye').addClass('fa-eye-slash');
-        } else {
-            passwordInput.attr('type', 'password');
-            $(this).find('i').removeClass('fa-eye-slash').addClass('fa-eye');
-        }
-    });
+//         if (passwordInput.attr('type') === 'password') {
+//             passwordInput.attr('type', 'text');
+//             $(this).find('i').removeClass('fa-eye').addClass('fa-eye-slash');
+//         } else {
+//             passwordInput.attr('type', 'password');
+//             $(this).find('i').removeClass('fa-eye-slash').addClass('fa-eye');
+//         }
+//     });
 
-    // Smooth scroll khi click vào các mục trong sidebar
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
+//     // Smooth scroll khi click vào các mục trong sidebar
+//     $('a[href^="#"]').on('click', function (e) {
+//         e.preventDefault();
 
-        const target = this.hash;
-        const $target = $(target);
+//         const target = this.hash;
+//         const $target = $(target);
 
-        $('html, body').animate({
-            'scrollTop': $target.offset().top - 20
-        }, 800, 'swing');
-    });
-});
+//         $('html, body').animate({
+//             'scrollTop': $target.offset().top - 20
+//         }, 800, 'swing');
+//     });
+// });
 
 //Brand
 
